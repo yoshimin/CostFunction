f3397c739534e942212a3ce6ee73128fed3f25b2
yoshimi-shingai <yoshimi.shingai@linecorp.com>
2019-05-15 12:38:48 +0900

    Initial Commit

---
+++ CostFunction/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CostFunction
+//
+//  Created by SHINGAI YOSHIMI on 2019/05/15.
+//  Copyright © 2019 SHINGAI YOSHIMI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
